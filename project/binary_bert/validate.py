23e5c1f0006cfeedaeb20fb133f137f25c224039
plutasnyy <kamil.plucinski97@gmail.com>
2020-12-11 11:55:25 +0100

    revert binary bert changes

--- project/binary_bert/validate.py
+++ project/binary_bert/validate.py
@@ -4,6 +4,7 @@ import torch
 from datasets import load_dataset
 from sklearn.metrics import roc_auc_score
 from tqdm.contrib import tenumerate
+import numpy as np
 
 from project.binary_bert.utils import load_binary_bert
 
@@ -12,32 +13,35 @@ dataset = load_dataset("civil_comments", split='test')
 model, tokenizer, class_names = load_binary_bert()
 true, pred = defaultdict(list), defaultdict(list)
 model.eval()
-
-length = 5000
-results = {}
-
-for id, sample in tenumerate(dataset, total=length):
-    if id >= length:
-        break
-
-    with torch.no_grad():
-        inputs = tokenizer(sample['text'], return_tensors="pt", truncation=True, padding=True).to(model.device)
+with torch.no_grad():
+    for id, batch in tenumerate(dataset, total=2):
+        if id > 2:
+            break
+        inputs = tokenizer(
+            batch['text'], return_tensors="pt", truncation=True, padding=True
+        ).to(model.device)
         out = model(inputs['input_ids'])
         scores = torch.sigmoid(out[0]).cpu().detach().numpy()
+        results = {}
+        for i, cla in enumerate(class_names):
+            results[cla] = (
+                scores[0][i]
+                if isinstance(batch['text'], str)
+                else [scores[ex_i][i].tolist() for ex_i in range(len(scores))]
+            )
+            if cla == 'identity_hate':
+                batch_cla = 'identity_attack'
+            else:
+                batch_cla = cla
+            true[cla].append(int(batch[batch_cla] > 0.5))
+            pred[cla].append(results[cla])
 
-    for i, cla in enumerate(class_names):
-        results[cla] = (
-            scores[0][i]
-            if isinstance(sample['text'], str)
-            else [scores[ex_i][i].tolist() for ex_i in range(len(scores))]
-        )
-        sample_class = 'identity_attack' if cla == 'identity_hate' else cla
+    roc_auc = []
 
-        true[sample_class].append(int(sample[sample_class] > 0.5))
-        pred[sample_class].append(results[cla])
+    for i in class_names:
+        try:
+            roc_auc.append(roc_auc_score(true[i], pred[i]))
+        except ValueError:
+            print("Only one class present in y_true")
 
-for i in class_names:
-    try:
-        print(i, roc_auc_score(true[i], pred[i]))
-    except ValueError:
-        print(i, "Only one class present in y_true")
+    print(np.mean(roc_auc))
